f36ceb13958d4c04a4a36b317d0aadbb62770da4
loren950603 <loren840603@gmail.com>
2022-10-15 15:23:42 -0400

    add skill list view

--- mydevskills/main_app/models.py
+++ mydevskills/main_app/models.py
@@ -22,7 +22,7 @@ class Skill(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
-        return self.name
+        return self.description
 
     def get_absolute_url(self):
         return reverse('detail', kwargs={'skill_id': self.id})
